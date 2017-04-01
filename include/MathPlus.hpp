cb94ecc983288e549113d0775519aeddb62a31d5
Sergey Tyulenev <marleeeeeey@gmail.com>
2017-04-01 13:07:29 +0300

    add stdplus::derivative to MathPlus

--- include/MathPlus.hpp
+++ include/MathPlus.hpp
@@ -2,9 +2,16 @@
 #define MathPlus_h__
 
 #include "StdInclude.h"
+#include "MacrosPlus.hpp"
 
 namespace stdplus
 {
+
+    struct Point
+    {
+        double x;
+        double y;
+    };
 
     template<class T>
     T getRandom(const T & min, const T & max)
@@ -29,8 +36,84 @@ namespace stdplus
 
         return (T)ret;
     }
+    
+}
 
 
+inline stdplus::Point operator-(const stdplus::Point & p)
+{
+    stdplus::Point ret;
+    ret.x = -p.x;
+    ret.y = -p.y;
+    return ret;
+}
+
+inline stdplus::Point operator+(const stdplus::Point & p1, const stdplus::Point & p2)
+{
+    stdplus::Point ret;
+    ret.x = p1.x + p2.x;
+    ret.y = p1.y + p2.y;
+    return ret;
+}
+
+inline stdplus::Point operator-(const stdplus::Point & p1, const stdplus::Point & p2)
+{
+    stdplus::Point ret = p1 + (-p2);
+    return ret;
+}
+
+inline stdplus::Point operator/(const stdplus::Point & p1, const double & div)
+{
+    stdplus::Point ret;
+    ret.x = p1.x / div;
+    ret.y = p1.y / div;
+    return ret;
+}
+
+inline std::ostream & operator<<(std::ostream & os, const stdplus::Point & p)
+{
+    os << "(" << p.x << ", " << p.y << ")";
+    return os;
+}
+
+
+namespace stdplus
+{
+    inline double derivative(stdplus::Point p1, stdplus::Point p2)
+    {
+        stdplus::Point delta = p2 - p1;        
+        AEXCEPT_IF(delta.x == 0);
+        return delta.y / delta.x;
+    }
+
+    inline double derivative(stdplus::Point p1, stdplus::Point p2, stdplus::Point p3)
+    {
+        stdplus::Point p23 = (p2 + p3) / 2;
+        return derivative(p1, p23);
+    }
+
+    inline std::vector<double> derivative(const std::vector<double> & inVec)
+    {
+        std::vector<double> outVec;
+
+        if (inVec.empty()) return outVec;
+
+        std::vector<double> extVec;
+        extVec.push_back(inVec.front());    // + 1
+        extVec.insert(extVec.end(), inVec.begin(), inVec.end());
+        extVec.push_back(inVec.back());     // + 1 --> 1+1 = 2
+
+        int index = 0;
+        for (auto it = extVec.begin(); it < extVec.end() - 2; it++)
+        {
+            stdplus::Point p1{ index + 0, *(it + 0) };
+            stdplus::Point p2{ index + 1, *(it + 1) };
+            stdplus::Point p3{ index + 2, *(it + 2) };
+            outVec.push_back(derivative(p1, p2, p3));
+        }
+
+        return outVec;
+    }
 
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,5 @@
 #include "StdPlus.h"
 
-struct Point
-{
-    double x;
-    double y;
-};
-
-Point operator-(const Point & p)
-{
-    Point ret;
-    ret.x = -p.x;
-    ret.y = -p.y;
-    return ret;
-}
-
-Point operator+(const Point & p1, const Point & p2)
-{
-    Point ret;
-    ret.x = p1.x + p2.x;
-    ret.y = p1.y + p2.y;
-    return ret;
-}
-
-Point operator-(const Point & p1, const Point & p2)
-{
-    Point ret = p1 + (-p2);
-    return ret;
-}
-
-Point operator/(const Point & p1, const double & div)
-{
-    Point ret;
-    ret.x = p1.x / div;
-    ret.y = p1.y / div;
-    return ret;
-}
-
-double derivative(Point p1, Point p2)
-{
-    Point delta = p2 - p1;
-    if (delta.x == 0)
-    {
-        delta.x = 0.0000001;
-        AMSG("WARNING in " + AFUNSIG);
-    }
-
-    return delta.y / delta.x;
-}
-
-double derivative(Point p1, Point p2, Point p3)
-{
-    Point p23 = (p2 + p3) / 2;
-    return derivative(p1, p23);
-}
-
-std::vector<double> derivative(const std::vector<double> & inVec)
-{
-    std::vector<double> outVec;
-
-    if (inVec.empty()) return outVec;
-
-    std::vector<double> extVec;
-    extVec.push_back(inVec.front());    // + 1
-    extVec.insert(extVec.end(), inVec.begin(), inVec.end());
-    extVec.push_back(inVec.back());     // + 1 --> 1+1 = 2
-
-    int index = 0;
-    for (auto it = extVec.begin(); it < extVec.end() - 2; it++)
-    {
-        Point p1{ index + 0, *(it + 0) };
-        Point p2{ index + 1, *(it + 1) };
-        Point p3{ index + 2, *(it + 2) };
-        outVec.push_back(derivative(p1, p2, p3));
-    }
-
-    return outVec;
-}
-
-std::ostream & operator<<(std::ostream & os, const Point & p)
-{
-    os << "(" << p.x << ", " << p.y << ")";
-    return os;
-}
-
 int main()
 {
 
@@ -97,21 +14,8 @@ int main()
     });
 
     stdplus::appendVecToFile("test.txt", genVars, "genVars");
-
-//     Point p1{ 0, 0 };
-//     Point p2{ 3, 4 };
-//     Point p3{ 9, 0 };
-//     AVAR(p1);
-//     AVAR(p2);
-//     AVAR(p3);
-// 
-//     double der2 = derivative(p1, p2);
-//     AVAR(der2);
-// 
-//     double der3 = derivative(p1, p2, p3);
-//     AVAR(der3);
-
-    std::vector<double> derVec = derivative(genVars);
+    
+    std::vector<double> derVec = stdplus::derivative(genVars);
     stdplus::appendVecToFile("test.txt", derVec, "derVec");    
 
     APAUSE;
