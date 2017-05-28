0108b8b65c68bf56257c46189ac2f93ca4971f9e
Sergey Tyulenev <marleeeeeey@gmail.com>
2017-05-28 03:54:02 +0300

    modify timeStr

--- include/StdPlus/TimePlus.hpp
+++ include/StdPlus/TimePlus.hpp
@@ -7,27 +7,34 @@
 
 namespace stdplus
 {
-    inline std::string dateStr()
+    inline tm curTimeInfo()
     {
         time_t t = ::time(0);   // get time now
         tm timeInfo;
         localtime_s(&timeInfo, &t);
 
-        enum { stringSize = 12 };
+        return timeInfo;
+    }
+
+    inline std::string dateStr()
+    {
+        enum { stringSize = 12 }; // TODO Why 12? (10+\0)=11
         char cString[stringSize];
-        strftime(cString, stringSize, "%Y-%m-%d", &timeInfo);
-
+        strftime(cString, stringSize, "%Y-%m-%d", &curTimeInfo());
         std::string retStr(cString);
-
         return retStr;
     }
 
 #ifndef WindowsPlus_h__
 
-	inline std::string timeStr()
-	{
-		return "00-00-00-000";
-	}
+    inline std::string timeStr()
+    {
+        enum { stringSize = 12 }; // TODO Why 12? 
+        char cString[stringSize];
+        strftime(cString, stringSize, "%T", &curTimeInfo());
+        std::string retStr(cString);
+        return retStr;
+    }
 
 #endif
 
