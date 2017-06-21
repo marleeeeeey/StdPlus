142dad79b60bd2f9445318d1d9f2868940f50124
Sergey Tyulenev <marleeeeeey@gmail.com>
2017-06-21 15:07:28 +0300

    teste dll support with different compile versions

--- include/StdPlus/Ones.h
+++ include/StdPlus/Ones.h
@@ -9,11 +9,16 @@
 
 
 #if defined(STD_PLUS_DLL)    
+#  pragma message("USED STD_PLUS_DLL")
 #  if defined(STD_PLUS_EXPORT)
+#    pragma message("USED EXPORT API")
 #    define STD_PLUS_API __declspec(dllexport)
 #  else
+#    pragma message("USED IMPORT API")
 #    define STD_PLUS_API __declspec(dllimport)  
 #  endif
+
+#  pragma comment(lib, "StdPlus.lib")
 
 namespace stdplus
 {
