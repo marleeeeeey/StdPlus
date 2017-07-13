a8ad5900c140ba3e7c48ccaffb7466f4470fb01c
Sergey Tyulenev <marleeeeeey@gmail.com>
2017-07-13 13:07:46 +0300

    add getCurrentMS

--- include/StdPlus/TimePlus.hpp
+++ include/StdPlus/TimePlus.hpp
@@ -9,6 +9,7 @@
 
 
 #include "StdInclude.h"
+#include <windows.h>        // GetSystemTime(...)
 
 
 namespace stdplus
@@ -31,22 +32,42 @@ namespace stdplus
         return retStr;
     }
 
-#ifndef STD_PLUS_WINDOWS_INCLUDE
+
+    inline int getCurrentMS()
+    {
+        SYSTEMTIME st;
+        // WINDOWS ONLY
+        GetSystemTime(&st);
+
+        return st.wMilliseconds;
+    }
+
 
     inline std::string timeStr()
     {
         enum { stringSize = 12 }; // TODO Why 12? 
         char cString[stringSize];
-        strftime(cString, stringSize, "%T", &curTimeInfo());
+
+        tm timeInfo = curTimeInfo();
+
+        strftime(cString, stringSize, "%T", &timeInfo);
         std::string retStr(cString);
+
+        std::replace(retStr.begin(), retStr.end(), ':', '-');
+        
+        retStr += "-" + std::to_string(getCurrentMS());
+
         return retStr;
     }
-
-#endif // STD_PLUS_WINDOWS_INCLUDE
 
     inline std::string dateTimeStr()
     {
         return dateStr() + " " + timeStr();
+    }
+
+    inline std::string getLogFileName(const std::string & mainName)
+    {
+        return stdplus::dateTimeStr() + " " + mainName;
     }
 
 }
