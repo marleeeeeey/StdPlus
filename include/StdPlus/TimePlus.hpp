458e97df8847ccb14dc13dc05a051d633d66d25b
Sergey Tyulenev <marleeeeeey@gmail.com>
2017-07-13 15:59:08 +0300

    add stdplus::Time struct

--- include/StdPlus/TimePlus.hpp
+++ include/StdPlus/TimePlus.hpp
@@ -11,58 +11,88 @@
 #include "StdInclude.h"
 #include <windows.h>        // GetSystemTime(...)
 
+namespace stdplus
+{
+    struct Time
+    {
+        int year;
+        int month;
+        int day;
+        int hour;
+        int minute;
+        int second;
+        int millisecond;
+    };
+}
+
+inline std::ostream & operator<<(std::ostream & os, const stdplus::Time & d)
+{
+    os
+        << std::setw(4) << std::setfill('0') << d.year        << "-"
+        << std::setw(2) << std::setfill('0') << d.month       << "-"
+        << std::setw(2) << std::setfill('0') << d.day         << " "
+        << std::setw(2) << std::setfill('0') << d.hour        << "-"
+        << std::setw(2) << std::setfill('0') << d.minute      << "-"
+        << std::setw(2) << std::setfill('0') << d.second      << "-"
+        << std::setw(3) << std::setfill('0') << d.millisecond 
+        ;
+
+    return os;
+}
 
 namespace stdplus
 {
-    inline tm curTimeInfo()
-    {
-        time_t t = ::time(0);   // get time now
-        tm timeInfo;
-        localtime_s(&timeInfo, &t);
 
-        return timeInfo;
+    inline Time getCurrentTime()
+    {
+        SYSTEMTIME st;        
+        GetSystemTime(&st); // WINDOWS ONLY
+
+        Time t = { 0 };
+        t.year = st.wYear;
+        t.month = st.wMonth;
+        t.day = st.wDay;
+        t.hour = st.wHour;
+        t.minute = st.wMinute;
+        t.second = st.wSecond;
+        t.millisecond = st.wMilliseconds;
+
+        return t;
     }
 
-    inline std::string dateStr()
+    inline std::string dateStr(const char splitter = '-')
     {
-        enum { stringSize = 12 }; // TODO Why 12? (10+\0)=11
-        char cString[stringSize];
-        strftime(cString, stringSize, "%Y-%m-%d", &curTimeInfo());
-        std::string retStr(cString);
-        return retStr;
+        Time t = getCurrentTime();
+
+        std::ostringstream ss;
+        ss 
+            << std::setw(4) << std::setfill('0') << t.year  << splitter 
+            << std::setw(2) << std::setfill('0') << t.month << splitter 
+            << std::setw(2) << std::setfill('0') << t.day
+            ;
+
+        return ss.str();
+    }
+       
+
+    inline std::string timeStr(const char splitter = '-')
+    {
+        Time t = getCurrentTime();
+
+        std::ostringstream ss;
+        ss 
+            << std::setw(2) << std::setfill('0') << t.hour   << splitter 
+            << std::setw(2) << std::setfill('0') << t.minute << splitter
+            << std::setw(2) << std::setfill('0') << t.second << splitter
+            << std::setw(3) << std::setfill('0') << t.millisecond
+            ;
+
+        return ss.str();
     }
 
-
-    inline int getCurrentMS()
+    inline std::string dateTimeStr(const char dateSplitter = '-', const char timeSplitter = '-')
     {
-        SYSTEMTIME st;
-        // WINDOWS ONLY
-        GetSystemTime(&st);
-
-        return st.wMilliseconds;
-    }
-
-
-    inline std::string timeStr()
-    {
-        enum { stringSize = 12 }; // TODO Why 12? 
-        char cString[stringSize];
-
-        tm timeInfo = curTimeInfo();
-
-        strftime(cString, stringSize, "%T", &timeInfo);
-        std::string retStr(cString);
-
-        std::replace(retStr.begin(), retStr.end(), ':', '-');
-        
-        retStr += "-" + std::to_string(getCurrentMS());
-
-        return retStr;
-    }
-
-    inline std::string dateTimeStr()
-    {
-        return dateStr() + " " + timeStr();
+        return dateStr(dateSplitter) + " " + timeStr(timeSplitter);
     }
 
     inline std::string getLogFileName(const std::string & mainName)
