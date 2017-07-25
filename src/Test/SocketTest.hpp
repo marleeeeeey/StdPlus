6f795a7d044e5cd05f46755088ee29bbb306e076
Sergey Tyulenev <marleeeeeey@gmail.com>
2017-07-25 04:39:37 +0300

    add test for socket

--- src/Test/SocketTest.hpp
+++ src/Test/SocketTest.hpp
@@ -5,4 +5,22 @@
 #include "StdPlus/StdPlus.h"
 
 
+TEST(TCPSocket, NonBlockTest)
+{
+    SocketUtil::StaticInit();
 
+    TCPSocketPtr sock = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
+    SocketAddress sa("127.0.0.1", 34567);
+    sock->Connect(sa);
+
+    sock->SetNonBlockingMode(true);
+
+    for (int i = 0; i < 30; ++i)
+    {
+        char ch = 'a' + i;
+        LOG_STD_VAR(ch);
+        sock->Send(&ch, sizeof(ch));
+    }
+
+    SocketUtil::CleanUp();
+}
