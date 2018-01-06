e2317c6d8f640dda35104bbb95e4b1abd0b4068a
Sergey Tyulenev <marleeeeeey@gmail.com>
2018-01-06 09:21:27 +0300

    removed

--- include/OopSocket/OopSocket.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "SocketAddress.h"
-//#include "SocketAddressFactory.h"
-#include "SocketUtil.h"
-#include "TCPSocket.h"
-#include "BaseTcpTransfer.hpp"
-#include "UDPSocket.h"
